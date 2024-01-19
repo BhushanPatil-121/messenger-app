0b92aed9e23551bcf49378f19ce2e56a34ae734f
sameer.edlabadkar <sameer.edlabadkar@datamato.com>
2024-01-19 23:26:41 +0530

    updated UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,12 @@ import './globals.css'
 import AuthContext from './context/AuthContext'
 import ActiveStatus from './components/ActiveStatus'
 import ToasterContext from './context/ToasterContext'
+import Head from 'next/head'
 
 export const metadata = {
   title: 'Messenger',
   description: 'Messenger Clone',
+  
 }
 
 export default function RootLayout({
@@ -15,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+            <link rel="icon" href="/images/chatapplogo.png" type="image/x-icon" />
+          </Head>
       <body>
         <AuthContext>
           <ToasterContext />
